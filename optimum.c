2c76845d58e3b3985880f77ee0de9348b61d26f6
Sushmithagowda2003 <122030217+Sushmithagowda2003@users.noreply.github.com>
2023-07-27 06:50:27 +0000

    yes

--- optimum.c
+++ optimum.c
@@ -21,3 +21,75 @@ void printframes(int frames[],int n)
     }
     printf("\n");
 }
+void optimal(int pages[],int n,int capacity)
+{
+    int frames[capacity];
+    int pagefaults=0;
+    int index,farthest,futureindex;
+    for(int i=0;i<capacity;i++)
+    frames[i]=-1;
+    for(int i=0;i<n;i++)
+    {
+        int page=pages[i];
+        index=findindex(frames,capacity,page);
+        if(index==-1)
+        {
+            int emptyindex=findindex(frames,capacity,-1);
+            if(emptyindex!=-1)
+            {
+                frames[emptyindex]=page;
+
+            }
+            else
+            {
+                farthest=i+1;
+                futureindex=-1;
+                for(int j=0;j<capacity;j++)
+                {
+                    int currentpage=frames[j];
+                    int k;
+                    for(k=i+1;k<n;k++)
+                    {
+                        if(currentpage==pages[k])
+                        {
+                            if(k>farthest)
+                            {
+                                farthest=k;
+                                futureindex=j;
+                            }
+                            break;
+                        }
+                    }
+                    if(k==n)
+                    {
+                        futureindex=j;
+                        break;
+                    }
+                }
+                frames[futureindex]=page;
+            }
+            pagefaults++;
+        }
+        printframes(frames,capacity);
+    }
+    printf("optimal page faults: %d\n",pagefaults);
+
+}
+int main()
+{
+    int pages[]={1,2,3,4,1,5,6,7,8,7,8,9,7,8,9,5,4,5,4,2};
+    int capacity=3;
+    int n=sizeof(pages)/sizeof(pages[0]);
+    printf("page reference sequence:\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",pages[i]);
+
+    }
+    printf("\n\n");
+    printf("\n");
+    printf("optimal algorithm:\n");
+    optimal(pages,n,capacity);
+    printf("\n");
+    return 0;
+}
